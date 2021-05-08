e6ebda54b495875c38010a9980ac24b2eccea04c
Zakir Hussain <itsjakirhussain@gmail.com>
2021-05-08 17:33:37 +0530

    add CSV file reading

---
+++ src/config.js
@@ -0,0 +1,5 @@
+const config = {
+    filePath: "../assets/min-transactions-test.csv",
+    exchangeRatesAPI: "https://min-api.cryptocompare.com/data/pricemulti?fsyms=BTC,ETH&tsyms=USD&api_key=8a40e2ade21cbdf645ae47836bae9dcc5acf206b3296497542f8d44ac961bba5"
+}
+export default config;
--- src/index.js
+++ src/index.js
@@ -1,4 +1,8 @@
+import csv from 'csv-parser';
+import config from './config';
+import { resolve } from 'path';
 import { Command } from 'commander';
+import { createReadStream } from 'fs';
 import { validateDate, validateToken } from './utils';
 
 const program = new Command();
@@ -7,11 +11,15 @@ program
   .helpOption('-h, --HELP', 'custom help message')
   .option('-d, --date <seconds>', 'Integer number of seconds since the Epoch', validateDate)
   .option('-t, --token <symbol>', 'The crypto token symbol', validateToken);
-  
+
 program.parse(process.argv);
 const options = program.opts();
 
 console.log(options);
+
+createReadStream(resolve(__dirname, config.filePath))
+  .pipe(csv())
+  .on('data', (each) => {console.log(each)})
 
 export default {
   program,
